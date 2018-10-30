bf388b3cd68781ef3da5dfecc33090512b782938
jairo <kidandcat@gmail.com>
2018-10-30 10:57:59 +0100

    change name

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,13 +8,13 @@ const s = {
     top: 0,
     left: 0,
     width: "100%",
-    backgroundColor: 'rgba(255,255,255,0.75)'
+    backgroundColor: "rgba(255,255,255,0.75)"
   },
   title: {
     flexGrow: 1,
     textAlign: "center",
-    color: 'rgb(0, 196, 189)',
-    fontSize: '3em'
+    color: "rgb(0, 196, 189)",
+    fontSize: "3em"
   },
   content: {
     width: "100%",
@@ -36,7 +36,7 @@ export default class Layout extends React.Component {
     const pattern = Trianglify({
       width: window.innerWidth,
       height: window.innerHeight,
-      x_colors: 'BuGn',
+      x_colors: "BuGn"
     });
     document.querySelector("#background").innerHTML = "";
     document.querySelector("#background").appendChild(pattern.canvas());
@@ -45,25 +45,23 @@ export default class Layout extends React.Component {
     return (
       <div>
         <Head>
-          <title>Artifact</title>
+          <title>Fragment Games</title>
           <link
             href="https://fonts.googleapis.com/css?family=Lato"
             rel="stylesheet"
           />
         </Head>
         <style jsx global>{`
-                html { 
-                    font-family: 'Lato', sans-serif;
-                }
-                body {
-                    margin: 0;
-                }
-            `}</style>
+          html {
+            font-family: "Lato", sans-serif;
+          }
+          body {
+            margin: 0;
+          }
+        `}</style>
         <AppBar position="static" style={s.navbar} color="default">
           <Toolbar>
-            <Typography style={s.title}>
-              Artifact
-            </Typography>
+            <Typography style={s.title}>Fragment Games</Typography>
           </Toolbar>
         </AppBar>
         <div style={s.content}>{this.props.children}</div>
